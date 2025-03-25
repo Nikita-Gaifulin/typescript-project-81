942e9a0766f6d2d805303f429021d00c383e431a
Nikita Gayfulin <Nikita.Gayfulin@metran.ru>
2025-03-25 23:30:50 +0300

    HexletCode

---
+++ src/HexletCode.ts
@@ -0,0 +1,10 @@
+class HexletCode
+{
+  formFor(template: { name: string, job: string, gender: string }, options: { url?: string }, _callback: (f: unknown) => void): string {
+    const action = options.url || '#'
+    const method = 'post'
+    return `<form action="${action}" method="${method}"></form>`
+  }
+}
+
+export default HexletCode
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Tag from './Tag.js'
+import HexletCode from './HexletCode.js'
 
 console.log(new Tag('br').toString())
 // <br>
@@ -18,3 +19,15 @@ console.log(new Tag('label', { for: 'email' }, 'Email').toString())
 
 console.log(new Tag('div').toString())
 // <div></div>
+
+const template = { name: 'rob', job: 'hexlet', gender: 'm' }
+let form = new HexletCode().formFor(template, {}, () => {})
+// <form action="#" method="post"></form>
+
+console.log(form);
+
+form = new HexletCode().formFor(template, {url: '/users'}, () => {});
+// <form action="/users" method="post"></form>
+
+
+console.log(form)
