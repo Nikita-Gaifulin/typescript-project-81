49c76b728183efbd6a94b53d05b5d8b7c0d02b80
Nikita Gayfulin <Nikita.Gayfulin@metran.ru>
2025-04-02 10:35:57 +0300

    fixed comments - remove unused dependencies

---
+++ .dist/src/Form.js
@@ -0,0 +1,52 @@
+class Form {
+    formFields = [];
+    template;
+    formFor(template, options, _callback) {
+        this.template = template;
+        const form = new Form();
+        form.template = this.template;
+        _callback(form);
+        const action = options.url ?? '#';
+        const method = options.method ?? 'post';
+        const fieldsHtml = form.formFields.join('\n');
+        // Если нет полей, возвращаем форму без переносов
+        if (form.formFields.length === 0) {
+            return `<form action="${action}" method="${method}"></form>`;
+        }
+        return `<form action="${action}" method="${method}">\n${fieldsHtml}\n</form>`;
+    }
+    input(fieldName, attributes = {}) {
+        if (!this.template || !(fieldName in this.template)) {
+            throw new Error(`Field '${fieldName}' does not exist in the template.`);
+        }
+        const fieldValue = this.template[fieldName];
+        const fieldType = attributes.as ?? 'input';
+        delete attributes.as;
+        if (fieldType === 'textarea') {
+            this.generateTextarea(fieldName, fieldValue, attributes);
+        }
+        else {
+            this.generateInput(fieldName, fieldValue, attributes);
+        }
+    }
+    submit(value) {
+        this.formFields.push(`<input type="submit" value="${value}">`);
+    }
+    generateInput(name, value, attributes) {
+        const attrs = this.buildAttributes({ ...attributes, type: 'text', name, value });
+        this.formFields.push(`<label for="${name}">${name.charAt(0).toUpperCase() + name.slice(1)}</label>\n<input${attrs}>`);
+    }
+    generateTextarea(name, value, attributes) {
+        const defaultAttrs = { cols: '20', rows: '40' };
+        const mergedAttrs = { ...defaultAttrs, ...attributes, name };
+        const attrs = this.buildAttributes(mergedAttrs);
+        this.formFields.push(`<textarea${attrs}>${value}</textarea>`);
+    }
+    buildAttributes(attributes) {
+        return Object.entries(attributes)
+            .filter(([_, value]) => value !== undefined)
+            .map(([key, value]) => ` ${key}="${value}"`)
+            .join('');
+    }
+}
+export default Form;
